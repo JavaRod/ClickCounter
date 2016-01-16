f20bed0c41471d220fbf890fa1da135dce39de2b
Rodrick Musser <rodrickmusser@Rodricks-iMac.local>
2016-01-16 12:04:22 -0800

    Added increment and decrement buttons

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -10,16 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //Label
+        var label = UILabel()
+        label.frame = CGRectMake(150,150,60,60)
+        label.text = "0"
+        
+        self.view.addSubview(label)
+        self.label = label
+        
+        //Button
+        var button = UIButton()
+        button.frame = CGRectMake(150,250,60,60)
+        button.setTitle("Click", forState: .Normal)
+        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(button)
+        
+        button.addTarget(self, action: "incrementalCount", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        //Decrement Button
+        var decButton = UIButton()
+        decButton.frame = CGRectMake(150,350,60,60)
+        decButton.setTitle("Click", forState: .Normal)
+        decButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        self.view.addSubview(decButton)
+        
+        decButton.addTarget(self, action: "decrementalCount", forControlEvents: UIControlEvents.TouchUpInside)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func incrementalCount() {
+        self.count++
+        self.label.text = "\(self.count)"
     }
-
+    
+    func decrementalCount() {
+        self.count--
+        self.label.text = "\(self.count)"
+    }
 
 }
 
